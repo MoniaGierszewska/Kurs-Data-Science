cca18bc15b4c9ed221ba7c5665ea9bd11486366b
Monika Gierszewska <mmm@gmail.com>
2023-03-13 14:23:50 +0100

    Dodano zadanie z manipulacji cytatem

---
+++ exercises/manipulacje_cytatami.py
@@ -0,0 +1,21 @@
+# Cytat Thomasa Watsona
+
+quote = "Myślę, że istnieje światowy rynek dla może pięciu komputerów."
+
+print("Oryginalny cytat w tłumaczeniu na język polski:")
+print(quote)
+
+print("\nDużymi literami:")
+print(quote.upper())
+
+print("\nMałymi literami:")
+print(quote.lower())
+
+print("\nWszystkie wyrazy od dużej litery:")
+print(quote.title())
+
+print("\nZ drobną zmianą:")
+print(quote.replace("pięciu", "milionów"))
+
+print("\nOryginalny cytat pozostał bez zmian:")
+print(quote)
