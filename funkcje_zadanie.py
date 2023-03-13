86cc188adc4b1feb7a30838366f641a2d423b288
Monika Gierszewska <mmm@gmail.com>
2023-03-13 13:54:25 +0100

    Notatka Dodano zadanie z funkcji

---
+++ funkcje_zadanie.py
@@ -0,0 +1,16 @@
+# Zadanie 1
+
+def is_even(x):
+    if x % 2 == 0:
+        print("True")
+    else:
+        print("False")
+is_even(5)
+
+# Zadanie 2
+
+def my_pow(x,y):
+    print("Potęga:", x**y)
+
+my_pow(2,5)
+
