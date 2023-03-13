547551c3cdb61809c94569eb44cb43cc1bcc5c35
Monika Gierszewska <mmm@gmail.com>
2023-03-13 13:55:50 +0100

    Notatka Dodano zadania z konwersji

---
+++ zmienne_konwersja_zadania.py
@@ -0,0 +1,13 @@
+# Zadanie 1
+
+a= float(input("Podaj cyfrę:"))
+b= float(input("Podaj kolejną cyfrę:"))
+print(a+b)
+print(a-b)
+print(a*b)
+
+# Zadanie 2
+
+nazwa=input("Podaj nazwę produktu:")
+ilosc=input("Podaj ilość produktu:")
+print("Produkt o nazwie: ", nazwa, " w ilości: ", ilosc)
