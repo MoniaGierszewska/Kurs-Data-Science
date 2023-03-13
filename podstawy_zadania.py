d67cebfeec3bc41884c4c6953ba4e00080210f7b
Monika Gierszewska <mmm@gmail.com>
2023-03-13 14:02:37 +0100

    Dodano zadania z podstaw

---
+++ podstawy_zadania.py
@@ -0,0 +1,24 @@
+# Zadanie 1
+
+x = 10
+y = 15
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(y%x)
+print(y//x)
+
+#Zadanie 2
+
+txt= "Monika ma zadanie"
+print(txt, "a kot ma pierdolca")
+
+print("Dodawanie:", x+y)
+x -= y
+print("Mnożenie", x*y)
+print("Dzielenie", x/y)
+print(x**y)
+print(y%x)
+print(y//x)
