5dc24295f1624cf4746c69a5763876fb8972373c
Monika Gierszewska <mmm@gmail.com>
2023-03-16 10:56:13 +0100

    Notatki zagnieżdzenie

---
+++ 15.03.2023/petle_zagniezdzenia.py
@@ -0,0 +1,36 @@
+# # Pętle i zagnieżdżenia
+#
+# accounts = [
+#     ("A001", "100600", 2500.50),
+#     ("B001", "200800", 1999.97),
+#     ("A002", "600100", 62780.00)]
+#
+# print(accounts)
+# print(accounts[-1])
+# print(accounts[-1][2])
+# print(accounts[0][2], accounts[1][2])
+#
+# # Pętle for
+# #1. Łańcuch znakowy
+#
+# for c in "KOTEK":
+#     print("Litera:",c)
+#
+# # 2. Łańcuch znakowy -> lista
+#
+# for word in "Ala ma kota".split(" "):
+#     print("Słowo:", word)
+#
+# # 3. Lista/Krotka
+#
+# for element in accounts:
+#     print ("Klient:", element)
+
+# 4.Ciąg liczb (range)
+# for i in range(2, 10):
+#     print("Liczba:", i)
+
+# 4a. Ciąg malejący
+for i in range (10,1,-1):
+    print(i)
+
