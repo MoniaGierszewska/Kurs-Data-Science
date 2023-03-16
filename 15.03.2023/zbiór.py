82aa72d8074399b5891b939f38699a9768aeb754
Monika Gierszewska <mmm@gmail.com>
2023-03-16 10:57:36 +0100

    Notatki zbiór

---
+++ 15.03.2023/zbiór.py
@@ -0,0 +1,9 @@
+# Zbiór
+numbers = {1,2,3,4,5,5,6,8,1,2,3,}
+
+#pusty zbiór: n=set()
+print(numbers)
+numbers.add(9) #dodanie jednego elementu
+numbers.update([1,4,5,2,0,11]) #dodanie listy elementów
+numbers.update(["a", "b", "c",])
+print(numbers)
